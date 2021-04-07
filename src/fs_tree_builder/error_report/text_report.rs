75d694ec0d6fb46df3353735b023a682ddd726e4
khai96_ <hvksmr1996@gmail.com>
2021-04-07 18:41:00 +0700

    refactor: Make `ErrorReport::text_report` testable

--- src/fs_tree_builder/error_report/text_report.rs
+++ src/fs_tree_builder/error_report/text_report.rs
@@ -1,13 +1,25 @@
 use super::ErrorReport;
+use std::fmt::{Display, Error, Formatter};
 
 impl<'a> ErrorReport<'a> {
     /// Prints error message in form of a line of text to stderr
     pub fn text_report(&self) {
-        eprintln!(
+        eprint!("{}", TextReport(self));
+    }
+}
+
+/// Wrapper around [`ErrorReport`] that `impl`s [`Display`]
+/// to make `ErrorReport::text_report` testable
+struct TextReport<'a>(&'a ErrorReport<'a>);
+
+impl<'a> Display for TextReport<'a> {
+    fn fmt(&self, formatter: &mut Formatter<'_>) -> Result<(), Error> {
+        writeln!(
+            formatter,
             "\r[error] {operation} {path:?}: {error}",
-            operation = self.operation.name(),
-            path = self.path,
-            error = self.error,
-        );
+            operation = self.0.operation.name(),
+            path = self.0.path,
+            error = self.0.error,
+        )
     }
 }
