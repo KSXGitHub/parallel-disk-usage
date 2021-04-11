ee857e5ad5d58b21dc4f8d771b931beb4387f105
khai96_ <hvksmr1996@gmail.com>
2021-04-11 22:51:29 +0700

    doc: Add documentation to app.rs

--- src/app.rs
+++ src/app.rs
@@ -1,17 +1,21 @@
 use super::Args;
 use structopt_utilities::StructOptUtils;
 
+/// The main application.
 pub struct App {
+    /// The CLI arguments.
     args: Args,
 }
 
 impl App {
+    /// Initialize the application from the environment.
     pub fn from_env() -> Self {
         App {
             args: Args::strict_from_args(),
         }
     }
 
+    /// Run the application.
     pub fn run(self) {
         let Args { copyright, files } = self.args;
 
