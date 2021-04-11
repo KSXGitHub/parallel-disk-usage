da7dc34ab9fc40ea08fc40cfb7e789e4b335fda4
khai96_ <hvksmr1996@gmail.com>
2021-04-11 22:54:32 +0700

    doc: Add documentation to args.rs

--- src/args.rs
+++ src/args.rs
@@ -1,15 +1,18 @@
 use std::path::PathBuf;
 use structopt::StructOpt;
 
+/// The CLI arguments.
 #[derive(Debug, Clone, StructOpt)]
 #[structopt(
     name = "dirt",
     about = "Summarize disk usage of the set of files, recursively for directories."
 )]
 pub struct Args {
+    /// Whether to print author information.
     #[structopt(long, about = "Print author information to stdout and exit.")]
     pub copyright: bool,
 
+    /// List of files and/or directories.
     #[structopt(name = "files", about = "List of files and/or directories.")]
     pub files: Vec<PathBuf>,
 }
