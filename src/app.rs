e1b1626f09f848a0a4d41f27f48579603ca5573c
khai96_ <hvksmr1996@gmail.com>
2021-04-14 12:19:22 +0700

    Replace --credits with long_about

--- src/app.rs
+++ src/app.rs
@@ -17,13 +17,7 @@ impl App {
 
     /// Run the application.
     pub fn run(self) {
-        let Args { credits, files } = self.args;
-
-        if credits {
-            println!("Apache-2.0 © 2021 Hoàng Văn Khải <https://ksxgithub.github.io/>");
-            println!("Donation: https://patreon.com/khai96_");
-            return;
-        }
+        let Args { files } = self.args;
 
         dbg!(files);
     }
--- src/args.rs
+++ src/args.rs
@@ -1,17 +1,20 @@
 use std::path::PathBuf;
 use structopt::StructOpt;
+use text_block_macros::text_block;
 
 /// The CLI arguments.
 #[derive(Debug, Clone, StructOpt)]
 #[structopt(
     name = "dirt",
-    about = "Summarize disk usage of the set of files, recursively for directories."
+
+    long_about = text_block! {
+        "Summarize disk usage of the set of files, recursively for directories."
+        ""
+        "Copyright: Apache-2.0 © 2021 Hoàng Văn Khải <https://ksxgithub.github.io/>"
+        "Donation: https://patreon.com/khai96_"
+    }
 )]
 pub struct Args {
-    /// Whether to print author information.
-    #[structopt(long, about = "Print author information to stdout and exit.")]
-    pub credits: bool,
-
     /// List of files and/or directories.
     #[structopt(name = "files", about = "List of files and/or directories.")]
     pub files: Vec<PathBuf>,
