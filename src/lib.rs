9419033360189fe121084b87cbc15be8ffb1d31c
khai96_ <hvksmr1996@gmail.com>
2021-04-16 09:17:47 +0700

    fmt: Run cargo fmt

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,10 @@
 pub mod app;
 pub mod args;
 pub mod fs_tree_builder;
+pub mod progress_report;
 pub mod size;
 pub mod tree;
 pub mod tree_builder;
-pub mod progress_report;
 
 pub use app::App;
 pub use args::Args;
