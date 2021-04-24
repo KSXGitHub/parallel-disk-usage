88e482b3eb9caeea83b2bc80fdcf6deca2f12fec
khai96_ <hvksmr1996@gmail.com>
2021-04-24 19:02:36 +0700

    Create Tree::iter_tree

--- src/tree/iter.rs
+++ src/tree/iter.rs
@@ -1,5 +1,7 @@
 pub mod iter_path;
+pub mod iter_tree;
 pub mod traverse;
 
 pub use iter_path::*;
+pub use iter_tree::*;
 pub use traverse::*;
---
+++ src/tree/iter/iter_tree.rs
@@ -0,0 +1,31 @@
+use super::{PostYield, TraverseIter, Yield};
+use crate::{size::Size, tree::Tree};
+
+/// The [`Yield`] type of `Tree::iter_tree`.
+pub struct IterTreeYield;
+impl<'a, Name, Data: Size> Yield<'a, Name, Data, ()> for IterTreeYield
+where
+    Tree<Name, Data>: 'a,
+{
+    type Item = &'a Tree<Name, Data>;
+
+    fn execute(&mut self, _: &mut (), tree: &'a Tree<Name, Data>) -> Self::Item {
+        tree
+    }
+}
+
+/// The [`PostYield`] type of `Tree::iter_tree`.
+pub struct IterTreePostYield;
+impl PostYield<()> for IterTreePostYield {
+    fn execute(&mut self, _: &mut ()) {}
+}
+
+type IterTreeResult<'a, Name, Data> =
+    TraverseIter<'a, Name, Data, (), IterTreeYield, IterTreePostYield>;
+
+impl<Name, Data: Size> Tree<Name, Data> {
+    /// Recursively traverse the tree.
+    pub fn iter_tree(&self) -> IterTreeResult<'_, Name, Data> {
+        self.traverse(IterTreeYield, IterTreePostYield)
+    }
+}
