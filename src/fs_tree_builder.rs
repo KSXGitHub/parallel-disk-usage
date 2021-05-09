0efa7483f4f3c327d29e916f7ccf69d65382968a
khai96_ <hvksmr1996@gmail.com>
2021-05-09 09:27:08 +0700

    doc: Create some links

--- src/fs_tree_builder.rs
+++ src/fs_tree_builder.rs
@@ -17,20 +17,20 @@ use std::os::unix::prelude::MetadataExt;
 /// Infers size from a [`Metadata`].
 pub type SizeGetter<Size> = fn(&Metadata) -> Size;
 
-/// Returns `metadata.len()`.
+/// Returns [`metadata.len()`](Metadata::len).
 #[inline]
 pub fn get_apparent_size<Data: Bytes>(metadata: &Metadata) -> Data {
     metadata.len().into()
 }
 
-/// Returns `metadata.blksize()` (POSIX only).
+/// Returns [`metadata.blksize()`](Metadata::blksize) (POSIX only).
 #[cfg(unix)]
 #[inline]
 pub fn get_block_size<Data: Bytes>(metadata: &Metadata) -> Data {
     metadata.blksize().into()
 }
 
-/// Returns `metadata.blocks()` (POSIX only).
+/// Returns [`metadata.blocks()`](Metadata::blocks) (POSIX only).
 #[cfg(unix)]
 #[inline]
 pub fn get_block_count(metadata: &Metadata) -> Blocks {
