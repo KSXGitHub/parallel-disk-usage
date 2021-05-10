170408b29c1bcd725002292e2c7b0de0cbd58581
khai96_ <hvksmr1996@gmail.com>
2021-05-10 19:44:21 +0700

    Move some functions from fs_tree_builder to size_getters

--- src/fs_tree_builder.rs
+++ src/fs_tree_builder.rs
@@ -1,6 +1,6 @@
 use super::{
     reporter::{error_report::Operation::*, ErrorReport, Event, Reporter},
-    size::{Blocks, Bytes, Size},
+    size::Size,
     tree::Tree,
     tree_builder::{Info, TreeBuilder},
 };
@@ -10,32 +10,6 @@ use std::{
     fs::{read_dir, symlink_metadata, Metadata},
     path::PathBuf,
 };
-
-#[cfg(unix)]
-use std::os::unix::prelude::MetadataExt;
-
-/// Infers size from a [`Metadata`].
-pub type SizeGetter<Size> = fn(&Metadata) -> Size;
-
-/// Returns [`metadata.len()`](Metadata::len).
-#[inline]
-pub fn get_apparent_size<Data: Bytes>(metadata: &Metadata) -> Data {
-    metadata.len().into()
-}
-
-/// Returns [`metadata.blksize()`](Metadata::blksize) (POSIX only).
-#[cfg(unix)]
-#[inline]
-pub fn get_block_size<Data: Bytes>(metadata: &Metadata) -> Data {
-    metadata.blksize().into()
-}
-
-/// Returns [`metadata.blocks()`](Metadata::blocks) (POSIX only).
-#[cfg(unix)]
-#[inline]
-pub fn get_block_count(metadata: &Metadata) -> Blocks {
-    metadata.blocks().into()
-}
 
 /// Build a [`Tree`] from a directory tree using [`From`] or [`Into`].
 #[derive(Debug)]
---
+++ src/size_getters.rs
@@ -0,0 +1,28 @@
+use super::size::{Blocks, Bytes};
+use std::fs::Metadata;
+
+#[cfg(unix)]
+use std::os::unix::prelude::MetadataExt;
+
+/// Infers size from a [`Metadata`].
+pub type SizeGetter<Size> = fn(&Metadata) -> Size;
+
+/// Returns [`metadata.len()`](Metadata::len).
+#[inline]
+pub fn get_apparent_size<Data: Bytes>(metadata: &Metadata) -> Data {
+    metadata.len().into()
+}
+
+/// Returns [`metadata.blksize()`](Metadata::blksize) (POSIX only).
+#[cfg(unix)]
+#[inline]
+pub fn get_block_size<Data: Bytes>(metadata: &Metadata) -> Data {
+    metadata.blksize().into()
+}
+
+/// Returns [`metadata.blocks()`](Metadata::blocks) (POSIX only).
+#[cfg(unix)]
+#[inline]
+pub fn get_block_count(metadata: &Metadata) -> Blocks {
+    metadata.blocks().into()
+}
