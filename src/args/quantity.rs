f253e2274a78ea04bd5f7a538c2ed530a24a5895
khai96_ <hvksmr1996@gmail.com>
2021-05-11 16:14:39 +0700

    cli: Change the possible values of --quantity

--- src/args/quantity.rs
+++ src/args/quantity.rs
@@ -2,18 +2,20 @@ use strum::{AsRefStr, EnumString, EnumVariantNames, VariantNames};
 
 /// Quantity to be measured.
 #[derive(Debug, Clone, Copy, PartialEq, Eq, AsRefStr, EnumString, EnumVariantNames)]
-#[strum(serialize_all = "kebab-case")]
 pub enum Quantity {
     /// Measure apparent sizes, equivalent to the
     /// [len](std::fs::Metadata::len) method.
+    #[strum(serialize = "len")]
     ApparentSize,
     /// Measure block sizes, equivalent to the
     /// [blksize](std::os::unix::prelude::MetadataExt::blksize) method (POSIX only).
     #[cfg(unix)]
+    #[strum(serialize = "blksize")]
     BlockSize,
     /// Count numbers of blocks, equivalent to the
     /// [blocks](std::os::unix::prelude::MetadataExt::blocks) method (POSIX only).
     #[cfg(unix)]
+    #[strum(serialize = "blocks")]
     BlockCount,
 }
 
