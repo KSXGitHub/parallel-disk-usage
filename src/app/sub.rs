30f659172cc1e6c6c9d99615aae095395024855d
khai96_ <hvksmr1996@gmail.com>
2021-05-12 18:10:15 +0700

    Assert that param.reduced_children is never empty

--- src/app/sub.rs
+++ src/app/sub.rs
@@ -93,6 +93,7 @@ where
         let minimal_ratio: f32 = minimal_ratio.into();
         let data_tree = if minimal_ratio > 0.0 {
             data_tree.par_partial_reduce_insignificant_data(minimal_ratio, |param| {
+                debug_assert!(!param.reduced_children.is_empty());
                 if param.reduced_children.len() == 1 {
                     param.reduced_children.first().unwrap().name().clone()
                 } else {
