66121b818be76c5d050daac30a9e6356d4517030
khai96_ <hvksmr1996@gmail.com>
2021-05-19 18:41:22 +0700

    ci: benchmark: Create script to report benchmark results to PR

---
+++ ci/github-actions/report-benchmark-result-pr.ts
@@ -0,0 +1,84 @@
+import { getOctokit, context } from '@actions/github'
+import console from 'console'
+import { readFileSync } from 'fs'
+import process from 'process'
+import { SELF_BENCHMARK_TOPICS, SelfBenchmarkTopic, parseSelfBenchmarkTopic } from './benchmark/matrix'
+import * as reportFiles from './benchmark/report-files'
+import * as env from './lib/env'
+
+const auth = env.load('GITHUB_TOKEN')
+const commitInfo = `* ref: ${context.issue.owner}/${context.issue.repo}@${context.sha}`
+
+function loadReport(topic: SelfBenchmarkTopic, ext: reportFiles.Extension) {
+  const { reportName } = parseSelfBenchmarkTopic(topic)
+  const filePath = reportFiles.getFileName(reportName, ext)
+  return readFileSync(filePath, 'utf-8')
+}
+
+function rendered(topic: SelfBenchmarkTopic) {
+  return [
+    '<details><summary>Rendered</summary>',
+    '',
+    loadReport(topic, 'md'),
+    '',
+    '</details>',
+  ].join('\n')
+}
+
+function codeBlock(topic: SelfBenchmarkTopic, summary: string, lang: reportFiles.Format) {
+  return [
+    '<details><summary>',
+    summary,
+    '</summary>',
+    '',
+    '```' + lang,
+    loadReport(topic, reportFiles.MAP[lang]),
+    '```',
+  ].join(' ')
+}
+
+function topicReport(topic: SelfBenchmarkTopic) {
+  const { commandSuffix } = parseSelfBenchmarkTopic(topic)
+
+  return [
+    '<details><summary>',
+    '',
+    '### ' + commandSuffix.join(' '),
+    '',
+    '</summary>',
+    '',
+    rendered(topic),
+    codeBlock(topic, 'Logs', 'log'),
+    codeBlock(topic, 'AsciiDoc', 'asciidoc'),
+    codeBlock(topic, 'CSV', 'csv'),
+    codeBlock(topic, 'JSON', 'json'),
+    codeBlock(topic, 'Markdown', 'markdown'),
+    '',
+    '</details>',
+  ].join(' ')
+}
+
+const overallReport = [
+  '## Benchmark Reports',
+  '',
+  commitInfo,
+  '',
+  ...SELF_BENCHMARK_TOPICS.map(topicReport),
+].join('\n')
+
+async function main() {
+  const github = getOctokit(auth).rest
+
+  // TODO: update created comment
+  await github.issues.createComment({
+    issue_number: context.issue.number,
+    owner: context.repo.owner,
+    repo: context.repo.repo,
+    body: overallReport,
+  })
+}
+
+main().catch(error => {
+  console.error(error)
+  throw process.exit(1)
+})
