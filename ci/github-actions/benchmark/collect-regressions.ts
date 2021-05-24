833bfaafe67be47978e40291a0d17c4946060459
khai96_ <hvksmr1996@gmail.com>
2021-05-24 16:18:30 +0700

    ci: benchmark: Carry categories

--- ci/github-actions/benchmark/collect-regressions.ts
+++ ci/github-actions/benchmark/collect-regressions.ts
@@ -1,13 +1,23 @@
-import { SELF_BENCHMARK_CATEGORIES, ACCEPTABLE_PERFORMANCE_REGRESSION, parseSelfBenchmarkCategory } from './matrix'
+import {
+  SELF_BENCHMARK_CATEGORIES,
+  ACCEPTABLE_PERFORMANCE_REGRESSION,
+  SelfBenchmarkCategory,
+  parseSelfBenchmarkCategory,
+} from './matrix'
 import * as reportFiles from './report-files'
 
-export function* collectRegressions(): Generator<readonly reportFiles.Regression[]> {
+export interface Item {
+  readonly category: SelfBenchmarkCategory
+  readonly regressions: readonly reportFiles.Regression[]
+}
+
+export function* collectRegressions(): Generator<Item> {
   for (const category of SELF_BENCHMARK_CATEGORIES) {
     const { reportName } = parseSelfBenchmarkCategory(category)
     const filePath = reportFiles.getFileName(reportName, 'json')
     const report = reportFiles.loadByPath(filePath)
     const regressions = [...reportFiles.detectRegressions(report, ACCEPTABLE_PERFORMANCE_REGRESSION)]
-    if (regressions.length) yield regressions
+    if (regressions.length) yield { category, regressions }
   }
 }
 
