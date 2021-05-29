6ecbc294645acb46cfc1b6a0f1affef9bcce83b0
khai96_ <hvksmr1996@gmail.com>
2021-05-29 19:33:55 +0700

    Make `test.sh` works on macOS

--- test.sh
+++ test.sh
@@ -1,18 +1,18 @@
 #! /bin/bash
 set -o errexit -o pipefail -o nounset
 
-run() {
+run() (
   echo >&2
   echo "exec> $*" >&2
   "$@"
-}
+)
 
-skip() {
+skip() (
   echo >&2
   echo "skip> $*" >&2
-}
+)
 
-run_if() {
+run_if() (
   condition="$1"
   shift
   case "$condition" in
@@ -23,23 +23,21 @@ run_if() {
     exit 1
     ;;
   esac
-}
+)
 
-flags=("$@")
-build_flags=()
-test_flags=()
-eval "build_flags+=(${BUILD_FLAGS:-})"
-eval "test_flags+=(${TEST_FLAGS:-})"
-unit() {
-  arguments=("$@" "${flags[@]}")
-  run_if "${LINT:-true}" cargo clippy "${arguments[@]}" -- -D warnings
-  run_if "${BUILD:-true}" cargo build "${build_flags[@]}" "${arguments[@]}"
-  run_if "${TEST:-true}" cargo test "${test_flags[@]}" "${arguments[@]}"
-}
+unit() (
+  build_flags=()
+  test_flags=()
+  eval "build_flags+=(${BUILD_FLAGS:-})"
+  eval "test_flags+=(${TEST_FLAGS:-})"
+  run_if "${LINT:-true}" cargo clippy "$@" -- -D warnings
+  run_if "${BUILD:-true}" cargo build "${build_flags[@]}" "$@"
+  run_if "${TEST:-true}" cargo test "${test_flags[@]}" "$@"
+)
 
 run_if "${FMT:-true}" cargo fmt
-unit
-unit --no-default-features
-unit --all-features
-unit --features cli
-unit --features cli-completions
+unit "$@"
+unit --no-default-features "$@"
+unit --all-features "$@"
+unit --features cli "$@"
+unit --features cli-completions "$@"
