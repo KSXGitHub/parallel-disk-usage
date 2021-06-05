8b96cb77012c00acb67a24c84211f76728b43fbe
khai96_ <hvksmr1996@gmail.com>
2021-06-05 15:11:02 +0700

    api+json: Reform `JsonData` into a struct

--- src/json_data.rs
+++ src/json_data.rs
@@ -7,14 +7,23 @@ use derive_more::{From, TryInto};
 #[cfg(feature = "json")]
 use serde::{Deserialize, Serialize};
 
-/// Output of the program with `--json-output` flag as well as
-/// input of the program with `--json-input` flag.
+/// The `"unit"` field and the `"tree"` field of [`JsonData`].
 #[derive(Debug, Clone, From, TryInto)]
 #[cfg_attr(feature = "json", derive(Deserialize, Serialize))]
 #[cfg_attr(feature = "json", serde(tag = "unit", content = "tree"))]
-pub enum JsonData {
+pub enum UnitAndTree {
     /// Tree where data is [bytes](Bytes).
     Bytes(Reflection<String, Bytes>),
     /// Tree where data is [blocks](Blocks).
     Blocks(Reflection<String, Blocks>),
+}
+
+/// Output of the program with `--json-output` flag as well as
+/// input of the program with `--json-input` flag.
+#[derive(Debug, Clone)]
+#[cfg_attr(feature = "json", derive(Deserialize, Serialize))]
+pub struct JsonData {
+    /// The `"unit"` field and the `"tree"` field.
+    #[cfg_attr(feature = "json", serde(flatten))]
+    pub unit_and_tree: UnitAndTree,
 }
