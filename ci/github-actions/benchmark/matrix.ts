4ee5a408fec72be2d01513059be7d87eea113b84
Sebastian Thiel <sebastian.thiel@icloud.com>
2023-05-05 19:07:41 +0200

    Setup benchmarks to disable hard-link counting where possible.
    According to PDUs limitations, it will count each hard-link to a file.
    As the correct handling of hard-links isn't free, I think it makes sense
    to count hard-links in all tools that support it.
    
    All of those which support it have the `-l` flag added to them. It was chosen
    over its long form to avoid making the command-line for `gdu` even longer and reduce
    the bar size the least amount possible.
    
    Please note that `ncdu` doesn't allow to disable hard-link counting, while `dust` doesn't
    support it either.

--- ci/github-actions/benchmark/matrix.ts
+++ ci/github-actions/benchmark/matrix.ts
@@ -94,10 +94,10 @@ export const COMPETING_BENCHMARK_MATRIX: readonly CompetingBenchmarkCategory[] =
     pduCliArgs: ['--quantity=apparent-size'],
     competitors: [
       ['dust', '--apparent-size'],
-      ['dua', '--apparent-size'],
+      ['dua', '-l', '--apparent-size'],
       ['ncdu', '-o', '/dev/stdout', '-0'],
-      ['gdu', '--show-apparent-size', '--non-interactive', '--no-progress'],
-      ['du', '--apparent-size'],
+      ['gdu', '-l', '--show-apparent-size', '--non-interactive', '--no-progress'],
+      ['du', '-l', '--apparent-size'],
     ],
   },
   {
@@ -105,10 +105,10 @@ export const COMPETING_BENCHMARK_MATRIX: readonly CompetingBenchmarkCategory[] =
     pduCliArgs: ['--quantity=block-size'],
     competitors: [
       ['dust'],
-      ['dua'],
+      ['dua', '-l'],
       ['ncdu', '-o', '/dev/stdout', '-0'],
-      ['gdu', '--non-interactive', '--no-progress'],
-      ['du'],
+      ['gdu', '-l', '--non-interactive', '--no-progress'],
+      ['du', '-l'],
     ],
   },
   {
@@ -123,8 +123,8 @@ export const COMPETING_BENCHMARK_MATRIX: readonly CompetingBenchmarkCategory[] =
     pduCliArgs: ['--max-depth=1'],
     competitors: [
       ['dutree', '--summary'],
-      ['dua'],
-      ['du', '--summarize'],
+      ['dua', '-l'],
+      ['du', '-l', '--summarize'],
     ],
   },
   {
@@ -133,27 +133,27 @@ export const COMPETING_BENCHMARK_MATRIX: readonly CompetingBenchmarkCategory[] =
     competitors: [
       ['dutree'],
       ['ncdu', '-o', '/dev/stdout', '-0'],
-      ['du', '--apparent-size'],
+      ['du', '-l', '--apparent-size'],
     ],
   },
   {
     id: 'no-sort',
     pduCliArgs: ['--no-sort'],
     competitors: [
-      ['du'],
-      ['dua'],
+      ['du', '-l'],
+      ['dua', '-l'],
       ['ncdu', '-o', '/dev/stdout', '-0'],
-      ['gdu', '--non-interactive', '--no-progress'],
+      ['gdu','-l', '--non-interactive', '--no-progress'],
     ],
   },
   {
     id: 'no-sort+summary',
     pduCliArgs: ['--no-sort', '--max-depth=1'],
     competitors: [
-      ['dua'],
+      ['dua', '-l'],
       ['ncdu', '-o', '/dev/null', '-0'],
-      ['gdu', '--non-interactive', '--no-progress'],
-      ['du', '--summarize'],
+      ['gdu', '-l', '--non-interactive', '--no-progress'],
+      ['du', '-l', '--summarize'],
     ],
   },
   {
@@ -161,7 +161,7 @@ export const COMPETING_BENCHMARK_MATRIX: readonly CompetingBenchmarkCategory[] =
     pduCliArgs: ['--progress'],
     competitors: [
       ['ncdu', '-o', '/dev/stdout', '-1'],
-      ['gdu', '--non-interactive'],
+      ['gdu', '-l', '--non-interactive'],
     ],
   },
 ]
