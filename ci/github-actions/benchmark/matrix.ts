470dbef1104d78f2d892b4b12e2b1f9bb9c33315
khai96_ <hvksmr1996@gmail.com>
2024-11-02 03:40:27 +0700

    perf: add 0.10.0 to benchmark

--- ci/github-actions/benchmark/matrix.ts
+++ ci/github-actions/benchmark/matrix.ts
@@ -40,6 +40,7 @@ export function parseSelfBenchmarkCategory(category: SelfBenchmarkCategory) {
 
 export const RELEASED_PDU_VERSIONS = [
   '0.9.0',
+  '0.10.0',
 ] as const
 
 export const ACCEPTABLE_PERFORMANCE_REGRESSION = 1.1 // 10%
