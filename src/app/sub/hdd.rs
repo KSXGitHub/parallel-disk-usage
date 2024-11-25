3e51cb2710bbb35bf6c91dd37bdd5dd6822be94e
khai96_ <hvksmr1996@gmail.com>
2024-11-25 18:28:56 +0700

    refactor: extract a function

--- src/app/sub/hdd.rs
+++ src/app/sub/hdd.rs
@@ -8,19 +8,26 @@ use sysinfo::DiskKind;
 pub fn any_path_is_in_hdd<Disk>(
     disks: &[Disk],
     paths: &[PathBuf],
-    get_disk_kind: impl Fn(&Disk) -> DiskKind,
+    get_disk_kind: impl Fn(&Disk) -> DiskKind + Copy,
     get_mount_point: impl Fn(&Disk) -> &Path + Copy,
 ) -> bool {
     paths
         .iter()
         .filter_map(|file| fs::canonicalize(file).ok())
-        .any(|path| {
-            if let Some(mount_point) = find_mount_point(&path, disks.iter().map(get_mount_point)) {
-                disks.iter().any(|disk| {
-                    get_disk_kind(disk) == DiskKind::HDD && get_mount_point(disk) == mount_point
-                })
-            } else {
-                false
-            }
+        .any(|path| path_is_in_hdd(&path, disks, get_disk_kind, get_mount_point))
+}
+
+pub fn path_is_in_hdd<Disk>(
+    path: &Path,
+    disks: &[Disk],
+    get_disk_kind: impl Fn(&Disk) -> DiskKind,
+    get_mount_point: impl Fn(&Disk) -> &Path + Copy,
+) -> bool {
+    if let Some(mount_point) = find_mount_point(path, disks.iter().map(get_mount_point)) {
+        disks.iter().any(|disk| {
+            get_disk_kind(disk) == DiskKind::HDD && get_mount_point(disk) == mount_point
         })
+    } else {
+        false
+    }
 }
