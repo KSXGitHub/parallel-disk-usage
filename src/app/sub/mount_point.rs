18f5f2a365da70d18bddb7ff7b128a1541da417a
Integral <integral@member.fsf.org>
2024-12-01 17:47:50 +0800

    feat: limit thread number to 1 when HDD is detected (#269)
    * feat: limit thread number to 1 when HDD is detected
    
    On HDD, multi-threaded du won't provide any performance benefit over
    the single-threaded one. Therefore, when HDD is detected on one of
    the files, limit the thread number to 1.
    
    This PR fixes issue #257.
    
    * Fix change requests
    
    * refactor: create a module
    
    * refactor: rename a test
    
    * refactor: unnecessary visibility scope
    
    * refactor: make `detect_hdd_in_files` testable
    
    * feat: improve warning messages
    
    * refactor: rename a function
    
    * refactor: rename an argument
    
    * refactor: extract a function
    
    * Add tests for path_is_in_hdd() & any_path_is_in_hdd()
    
    * Apply suggestions
    
    * Update src/app/sub/hdd.rs
    
    Co-authored-by: Khải <hvksmr1996@gmail.com>
    
    * Remove unused import
    
    * refactor: use mocked trait
    
    * style: remove unnecessary trailing comma
    
    * refactor: simplify the code
    
    * refactor: use constructor to reduce lines of code
    
    * test: add more cases
    
    * refactor: shorten the code
    
    * refactor: descriptive paths
    
    * refactor: rearrange
    
    * refactor: shorten the code
    
    * style: add trailing comma
    
    * refactor: early return
    
    * docs: add
    
    ---------
    
    Co-authored-by: khai96_ <hvksmr1996@gmail.com>

---
+++ src/app/sub/mount_point.rs
@@ -0,0 +1,39 @@
+use std::{ffi::OsStr, path::Path};
+
+/// Find a mount point that contains `absolute_path`.
+pub fn find_mount_point<'a>(
+    absolute_path: &Path,
+    all_mount_points: impl IntoIterator<Item = &'a Path>,
+) -> Option<&'a Path> {
+    all_mount_points
+        .into_iter()
+        .filter(|mnt| absolute_path.starts_with(mnt))
+        .max_by_key(|mnt| AsRef::<OsStr>::as_ref(mnt).len()) // Mount points can be nested in each other
+}
+
+#[cfg(test)]
+mod tests {
+    use super::find_mount_point;
+    use pretty_assertions::assert_eq;
+    use std::path::Path;
+
+    #[test]
+    fn test_mount_point() {
+        let all_mount_points = ["/", "/home", "/mnt/data", "/mnt/data/repo", "/mnt/repo"];
+
+        for (path, expected_mount_point) in &[
+            ("/etc/fstab", "/"),
+            ("/home/user", "/home"),
+            ("/mnt/data/repo/test", "/mnt/data/repo"),
+            ("/mnt/data/test/test", "/mnt/data/"),
+            ("/mnt/repo/test/test", "/mnt/repo/"),
+        ] {
+            println!("CASE: {path} → {expected_mount_point}");
+            let all_mount_points = all_mount_points.map(Path::new);
+            assert_eq!(
+                find_mount_point(Path::new(path), all_mount_points).unwrap(),
+                Path::new(expected_mount_point),
+            );
+        }
+    }
+}
