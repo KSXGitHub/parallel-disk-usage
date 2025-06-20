064d4a8d964e9f15cc4d005d91998697a350019d
khai96_ <hvksmr1996@gmail.com>
2025-06-20 20:59:40 +0700

    chore(deps): update clap-related dependencies

--- exports/completion.fish
+++ exports/completion.fish
@@ -1,5 +1,9 @@
-complete -c pdu -l bytes-format -d 'How to display the numbers of bytes' -r -f -a "{plain\t'Display plain number of bytes without units',metric\t'Use metric scale, i.e. 1K = 1000B, 1M = 1000K, and so on',binary\t'Use binary scale, i.e. 1K = 1024B, 1M = 1024K, and so on'}"
-complete -c pdu -l quantity -d 'Aspect of the files/directories to be measured' -r -f -a "{apparent-size\t'Measure apparent sizes',block-size\t'Measure block sizes (block-count * 512B)',block-count\t'Count numbers of blocks'}"
+complete -c pdu -l bytes-format -d 'How to display the numbers of bytes' -r -f -a "plain\t'Display plain number of bytes without units'
+metric\t'Use metric scale, i.e. 1K = 1000B, 1M = 1000K, and so on'
+binary\t'Use binary scale, i.e. 1K = 1024B, 1M = 1024K, and so on'"
+complete -c pdu -l quantity -d 'Aspect of the files/directories to be measured' -r -f -a "apparent-size\t'Measure apparent sizes'
+block-size\t'Measure block sizes (block-count * 512B)'
+block-count\t'Count numbers of blocks'"
 complete -c pdu -l max-depth -d 'Maximum depth to display the data (must be greater than 0)' -r
 complete -c pdu -l total-width -d 'Width of the visualization' -r
 complete -c pdu -l column-width -d 'Maximum widths of the tree column and width of the bar column' -r
