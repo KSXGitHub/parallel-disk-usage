51dc63972d6b5ebc23b7647e3a993a8e9befeabd
khai96_ <hvksmr1996@gmail.com>
2025-06-29 01:46:53 +0700

    perf: use 0.12.0 as the base for benchmark

--- ci/github-actions/benchmark/matrix.ts
+++ ci/github-actions/benchmark/matrix.ts
@@ -39,7 +39,7 @@ export function parseSelfBenchmarkCategory(category: SelfBenchmarkCategory) {
 }
 
 export const RELEASED_PDU_VERSIONS = [
-  '0.10.0',
+  '0.12.0',
 ] as const
 
 export const ACCEPTABLE_PERFORMANCE_REGRESSION = 1.1 // 10%
