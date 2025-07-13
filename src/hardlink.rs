2369f76eb495443ab5fc8194f3b507b6d282b3fc
khai96_ <hvksmr1996@gmail.com>
2025-07-14 05:51:29 +0700

    feat(api): expose `aware` and `ignorant`

--- src/hardlink.rs
+++ src/hardlink.rs
@@ -1,16 +1,15 @@
-mod ignorant;
-pub use ignorant::HardlinkIgnorant;
-
 // `RecordHardlink` is POSIX-exclusive, because whilst Windows does have `MetadataExt::number_of_links`, it requires Nightly.
 #[cfg(unix)]
-mod aware;
+pub mod aware;
 #[cfg(unix)]
 pub use aware::HardlinkAware;
 
 pub mod hardlink_list;
+pub mod ignorant;
 pub mod link_path_list;
 pub mod record;
 
 pub use hardlink_list::{HardlinkList, HardlinkListReflection};
+pub use ignorant::HardlinkIgnorant;
 pub use link_path_list::{LinkPathList, LinkPathListReflection};
 pub use record::{RecordHardlinks, RecordHardlinksArgument};
