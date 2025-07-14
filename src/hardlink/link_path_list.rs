f516d15de8f8ff27af3e0d41b99c004da8630134
khai96_ <hvksmr1996@gmail.com>
2025-07-14 13:50:23 +0700

    refactor: move iterator to its own module

--- src/hardlink/link_path_list.rs
+++ src/hardlink/link_path_list.rs
@@ -1,11 +1,12 @@
+mod iter;
 mod reflection;
 
+pub use iter::Iter;
 pub use reflection::Reflection;
 
 pub use Reflection as LinkPathListReflection;
 
-use pipe_trait::Pipe;
-use std::{iter::FusedIterator, path::PathBuf, slice};
+use std::path::PathBuf;
 
 /// List of different hardlinks to the same file.
 ///
@@ -38,67 +39,8 @@ impl LinkPathList {
         self.0.is_empty()
     }
 
-    /// Iterate over the paths inside the list.
-    pub fn iter(&self) -> Iter {
-        self.0.iter().pipe(Iter)
-    }
-
     /// Create reflection.
     pub fn into_reflection(self) -> Reflection {
         self.into()
     }
 }
-
-/// [Iterator] over the paths inside a [`LinkPathList`].
-#[derive(Debug, Clone)]
-pub struct Iter<'a>(slice::Iter<'a, PathBuf>);
-
-impl<'a> Iterator for Iter<'a> {
-    type Item = &'a PathBuf;
-
-    #[inline]
-    fn next(&mut self) -> Option<Self::Item> {
-        self.0.next()
-    }
-
-    #[inline]
-    fn size_hint(&self) -> (usize, Option<usize>) {
-        self.0.size_hint()
-    }
-
-    #[inline]
-    fn count(self) -> usize {
-        self.0.count()
-    }
-
-    #[inline]
-    fn nth(&mut self, n: usize) -> Option<Self::Item> {
-        self.0.nth(n)
-    }
-
-    #[inline]
-    fn last(self) -> Option<Self::Item> {
-        self.0.last()
-    }
-}
-
-impl<'a> DoubleEndedIterator for Iter<'a> {
-    #[inline]
-    fn next_back(&mut self) -> Option<Self::Item> {
-        self.0.next_back()
-    }
-
-    #[inline]
-    fn nth_back(&mut self, n: usize) -> Option<Self::Item> {
-        self.0.nth_back(n)
-    }
-}
-
-impl<'a> ExactSizeIterator for Iter<'a> {
-    #[inline]
-    fn len(&self) -> usize {
-        self.0.len()
-    }
-}
-
-impl FusedIterator for Iter<'_> {}
