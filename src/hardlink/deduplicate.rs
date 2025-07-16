fdea3698e79a517a56cc82eb1d92f46f725e97dc
khai96_ <hvksmr1996@gmail.com>
2025-07-16 23:43:56 +0700

    docs: I have decided that I don't care
    There's just too many ways to create invalid tree (such as implementing
    a custom trait incorrectly for example) that it's not worth the efforts

--- src/hardlink/deduplicate.rs
+++ src/hardlink/deduplicate.rs
@@ -1,10 +1,5 @@
 use crate::{data_tree::DataTree, os_string_display::OsStringDisplay, size};
 
-// TODO:
-// Problem: It is possible for the users to call deduplicate twice on 1 data tree, which is unsound.
-// Potential solution #1: Consider adding the data tree to the deduplication record as hidden field until it is processed.
-// Potential solution #2: Perhaps the deduplication should be done in FsTreeBuilder itself.
-
 /// Ability to correct the sizes in a [`DataTree`] by reducing the size of recorded shared links.
 ///
 /// The input tree is assumed to be not yet deduplicated.
