b669b90d308e02527dfc4f73140ee1e807d446c8
khai96_ <hvksmr1996@gmail.com>
2026-03-15 17:34:08 +0700

    refactor(devcontainer): rename some variables

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -9,9 +9,9 @@ npm install -g pnpm@7.33.7
 (cd ci/github-actions && pnpm install)
 
 echo "Installing hyperfine from GitHub release..." >&2
-HYPERFINE_VERSION="1.20.0"
-HYPERFINE_ARCHIVE="hyperfine-v${HYPERFINE_VERSION}-x86_64-unknown-linux-musl"
-HYPERFINE_URL="https://github.com/sharkdp/hyperfine/releases/download/v${HYPERFINE_VERSION}/${HYPERFINE_ARCHIVE}.tar.gz"
+hyperfine_version="1.20.0"
+hyperfine_archive_name="hyperfine-v${hyperfine_version}-x86_64-unknown-linux-musl"
+hyperfine_url="https://github.com/sharkdp/hyperfine/releases/download/v${hyperfine_version}/${hyperfine_archive_name}.tar.gz"
 mkdir -p "$HOME/.local/bin"
-curl -fsSL "$HYPERFINE_URL" \
-  | tar -xz --strip-components=1 -C "$HOME/.local/bin" "${HYPERFINE_ARCHIVE}/hyperfine"
+curl -fsSL "$hyperfine_url" \
+  | tar -xz --strip-components=1 -C "$HOME/.local/bin" "${hyperfine_archive_name}/hyperfine"
