959d7c68a651b45db721723c42ed8d34e9cc0fd5
khai96_ <hvksmr1996@gmail.com>
2026-03-15 17:34:40 +0700

    refactor(devcontainer): unify two lines

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -13,5 +13,4 @@ hyperfine_version="1.20.0"
 hyperfine_archive_name="hyperfine-v${hyperfine_version}-x86_64-unknown-linux-musl"
 hyperfine_url="https://github.com/sharkdp/hyperfine/releases/download/v${hyperfine_version}/${hyperfine_archive_name}.tar.gz"
 mkdir -p "$HOME/.local/bin"
-curl -fsSL "$hyperfine_url" \
-  | tar -xz --strip-components=1 -C "$HOME/.local/bin" "${hyperfine_archive_name}/hyperfine"
+curl -fsSL "$hyperfine_url" | tar -xz --strip-components=1 -C "$HOME/.local/bin" "${hyperfine_archive_name}/hyperfine"
